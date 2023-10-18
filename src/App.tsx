bb1ce988c0cbda2ba881fd2a66bd3ebf2c7d8693
Christian Paul <christian.paul@nordeck.net>
2023-10-18 13:04:36 +0800

    Board: useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 type sqares = string[]
 
@@ -22,7 +22,7 @@ interface BoardProps {
 }
 
 function Board({ xIsNext, squares, onPlay }: BoardProps) {
-  function handleClick(i: number) {
+  const handleClick = useCallBack((i: number) => {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -33,33 +33,28 @@ function Board({ xIsNext, squares, onPlay }: BoardProps) {
       nextSquares[i] = 'O';
     }
     onPlay(nextSquares);
-  }
+  }, [xIsNext, squares, onPlay]);
 
   const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
       <>
         <div className="status">{status}</div>
         <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square value={squares[0]} onSquareClick={useCallback(() => handleClick(0))} />
+          <Square value={squares[1]} onSquareClick={useCallback(() => handleClick(1))} />
+          <Square value={squares[2]} onSquareClick={useCallback(() => handleClick(2))} />
         </div>
         <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square value={squares[3]} onSquareClick={useCallback(() => handleClick(3))} />
+          <Square value={squares[4]} onSquareClick={useCallback(() => handleClick(4))} />
+          <Square value={squares[5]} onSquareClick={useCallback(() => handleClick(5))} />
         </div>
         <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          <Square value={squares[6]} onSquareClick={useCallback(() => handleClick(6))} />
+          <Square value={squares[7]} onSquareClick={useCallback(() => handleClick(7))} />
+          <Square value={squares[8]} onSquareClick={useCallback(() => handleClick(8))} />
         </div>
       </>
   );
